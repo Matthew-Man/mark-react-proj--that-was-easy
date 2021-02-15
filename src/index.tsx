1eb4e9d7e84ff8eba67683be21bbe7a5ad51bb89
Matthew Man <matthew08m@gmail.com>
2021-02-15 18:40:34 +0000

    multiple buttons  adeed

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,28 @@
+// import { create } from 'domain';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+const createButton = (option: string) => (
+  <button onClick={() => window.alert("that was " + option)}>{option}</button>
+)
+
+
+function buttonBase() {
+  const buttonOptions = ["ease", "ok", "difficult", "very difficult"]
+  return (
+    buttonOptions.map(createButton)
+  )
+}
+
+function CompileButtons() {
+  return (
+    <div>
+      {buttonBase()}
+    </div>
+  )
+}
+
+
+ReactDOM.render(<CompileButtons />, document.getElementById("button-base"))
